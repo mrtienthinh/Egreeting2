460733485ff631ac99be881cd392d6bf4f1c8dde
Mrtienthinh <mrtienthinh@gmail.com>
2019-10-13 09:52:01 +0700

    code admin egreeting role

--- Backend/Egreeting/Egreeting.Web/Controllers/Admin/EgreetingRolesController.cs
+++ Backend/Egreeting/Egreeting.Web/Controllers/Admin/EgreetingRolesController.cs
@@ -23,9 +23,23 @@ namespace Egreeting.Web.Controllers.Admin
         }
 
         // GET: EgreetingRoles
-        public ActionResult Index()
+        public ActionResult Index(string search, int page = 1, int pageSize = 10)
         {
-            return View(ViewNamesConstant.AdminEgreetingRolesIndex,EgreetingRoleBusiness.All.ToList());
+            var listModel = new List<EgreetingRole>();
+            if (!string.IsNullOrEmpty(search))
+            {
+                listModel = EgreetingRoleBusiness.All.Where(x => x.EgreetingRoleName.Contains(search)).OrderBy(x => x.EgreetingRoleID).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                ViewBag.totalItem = EgreetingRoleBusiness.All.Count(x => x.EgreetingRoleName.Contains(search));
+            }
+            else
+            {
+                ViewBag.totalItem = EgreetingRoleBusiness.All.Count();
+                listModel = EgreetingRoleBusiness.All.OrderBy(x => x.EgreetingRoleID).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            }
+            ViewBag.currentPage = page;
+            ViewBag.pageSize = pageSize;
+            ViewBag.search = search;
+            return View(ViewNamesConstant.AdminEgreetingRolesIndex,listModel);
         }
 
         // GET: EgreetingRoles/Details/5
@@ -54,7 +68,7 @@ namespace Egreeting.Web.Controllers.Admin
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "EgreetingRoleID,EgreetingRoleName,CreatedDate,ModifiedDate")] EgreetingRole egreetingRole)
+        public ActionResult Create([Bind(Include = "EgreetingRoleName")] EgreetingRole egreetingRole)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +100,7 @@ namespace Egreeting.Web.Controllers.Admin
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "EgreetingRoleID,EgreetingRoleName,CreatedDate,ModifiedDate")] EgreetingRole egreetingRole)
+        public ActionResult Edit([Bind(Include = "EgreetingRoleID,EgreetingRoleName")] EgreetingRole egreetingRole)
         {
             if (ModelState.IsValid)
             {
@@ -97,28 +111,12 @@ namespace Egreeting.Web.Controllers.Admin
             return View(ViewNamesConstant.AdminEgreetingRolesEdit,egreetingRole);
         }
 
-        // GET: EgreetingRoles/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EgreetingRole egreetingRole = EgreetingRoleBusiness.Find(id);
-            if (egreetingRole == null)
-            {
-                return HttpNotFound();
-            }
-            return View(ViewNamesConstant.AdminEgreetingRolesDelete,egreetingRole);
-        }
-
         // POST: EgreetingRoles/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int ItemID)
         {
-            EgreetingRole egreetingRole = EgreetingRoleBusiness.Find(id);
-            EgreetingRoleBusiness.Delete(egreetingRole);
+            EgreetingRoleBusiness.Delete(ItemID);
             EgreetingRoleBusiness.Save();
             return RedirectToAction("Index");
         }
