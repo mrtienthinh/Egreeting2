1bd3cd0ba0f5edf56204df7de162f25cff8fbaaf
Mrtienthinh <mrtienthinh@gmail.com>
2019-10-14 18:49:56 +0700

    create viewdata content

---
+++ Backend/Egreeting/Egreeting.Domain/ViewDataConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egreeting.Domain
+{
+    public static class ViewDataConstant
+    {
+        public static readonly string SELECT_LIST_ROLE = "SELECT_LIST_ROLE";
+    }
+}
