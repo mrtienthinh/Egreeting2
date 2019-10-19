95ecaa828a15ad3c1c9ce3729218914b01f109be
Mrtienthinh <mrtienthinh@gmail.com>
2019-10-19 23:10:07 +0700

    update seed role and user

--- Backend/Egreeting/Egreeting.Models/Migrations/Configuration.cs
+++ Backend/Egreeting/Egreeting.Models/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace Egreeting.Models.Migrations
 {
+    using Egreeting.Models.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -38,6 +41,64 @@ namespace Egreeting.Models.Migrations
                 new Models.EgreetingRole { EgreetingRoleName = "User"},
                 new Models.EgreetingRole { EgreetingRoleName = "Subcriber"}
             );
+
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Admin" };
+                manager.Create(role);
+            }
+            if (!context.Roles.Any(r => r.Name == "User"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "User" };
+                manager.Create(role);
+            }
+            if (!context.Roles.Any(r => r.Name == "Subcriber"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Subcriber" };
+                manager.Create(role);
+            }
+            if (!context.Users.Any(u => u.UserName == "mrtienthinh@gmail.com"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "mrtienthinh@gmail.com", Email = "mrtienthinh@gmail.com"};
+
+                manager.Create(user, "123456aA@");
+                manager.AddToRole(user.Id, "Admin");
+            }
+            if (!context.Users.Any(u => u.UserName == "admin@gmail.com"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "admin@gmail.com", Email = "admin@gmail.com" };
+
+                manager.Create(user, "123456aA@");
+                manager.AddToRole(user.Id, "Admin");
+            }
+            if (!context.Users.Any(u => u.UserName == "user@gmail.com"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "user@gmail.com", Email = "user@gmail.com" };
+
+                manager.Create(user, "123456aA@");
+                manager.AddToRole(user.Id, "User");
+            }
+            if (!context.Users.Any(u => u.UserName == "subcriber@gmail.com"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "subcriber@gmail.com", Email = "subcriber@gmail.com" };
+
+                manager.Create(user, "123456aA@");
+                manager.AddToRole(user.Id, "Subcriber");
+            }
         }
     }
 }
