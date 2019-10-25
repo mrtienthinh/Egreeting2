9b51a26152cf02ee316db790180d20a01954c917
Mrtienthinh <mrtienthinh@gmail.com>
2019-10-25 23:49:27 +0700

    add service send mail

---
+++ Backend/Egreeting/Egreeting.Web/Controllers/Admin/SendMailController.cs
@@ -0,0 +1,44 @@
+﻿using Egreeting.Models.AppContext;
+using Egreeting.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Egreeting.Web.Controllers.Admin
+{
+    public class SendMailController : BaseController
+    {
+
+        // GET: SendMail
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SendByOrder(int? ItemID)
+        {
+            Utils.Utils.SendMailByOrder(ItemID);
+            return RedirectToAction("Index","Orders");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Sendall()
+        {
+            using (var context = new EgreetingContext())
+            {
+                var ListItemID = context.Set<Order>().Where(x => x.ScheduleTime > DateTime.Now && x.Draft != null).Select(x => x.OrderID).ToList();
+                Utils.Utils.SendMailAll(ListItemID);
+            }
+            return RedirectToAction("Index", "Orders");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SendByOrderDetail(int? ItemID)
+        {
+            Utils.Utils.SendMailByOrderDetail(ItemID);
+            return RedirectToAction("Index", "Orders");
+        }
+    }
+}
--- Backend/Egreeting/Egreeting.Web/Utils/Utils.cs
+++ Backend/Egreeting/Egreeting.Web/Utils/Utils.cs
@@ -1,8 +1,12 @@
 ﻿using Egreeting.Domain;
+using Egreeting.Models.AppContext;
+using Egreeting.Models.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
+using System.Web.Hosting;
 
 namespace Egreeting.Web.Utils
 {
@@ -26,6 +30,96 @@ namespace Egreeting.Web.Utils
         public static bool CheckExtentionFile(this string extention)
         {
             return AcceptExtensionFile.ListAcceptPicture.Contains(extention.ToUpper());
+        }
+
+        public static void SendMailByOrder(int? ItemID)
+        {
+            using (var context = new EgreetingContext())
+            {
+                var order = context.Set<Order>().Find(ItemID);
+
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                foreach (var item in order.OrderDetails)
+                {
+                    Attachment data = new Attachment(HostingEnvironment.ApplicationPhysicalPath + "Uploads/EcardFiles/" + item.Ecard.EcardUrl);
+                    mail.Attachments.Add(data);
+                }
+
+                mail.From = new MailAddress("lethanh.hlht1993@gmail.com");
+                mail.To.Add(order.RecipientEmail);
+                mail.Subject = order.SendSubject + " - " + order.SenderName;
+                mail.Body = order.SendMessage;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("lethanh.hlht1993@gmail.com", "*******");
+                SmtpServer.EnableSsl = true;
+
+                try
+                {
+                    //SmtpServer.Send(mail);
+                    order.SendStatus = true;
+                    foreach (var item in order.OrderDetails)
+                    {
+                        item.SendStatus = true;
+                        item.SendTime = DateTime.Now;
+                    }
+                    context.Set<Order>().Attach(order);
+                    context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+
+
+
+        }
+        public static void SendMailByOrderDetail(int? ItemID)
+        {
+            using (var context = new EgreetingContext())
+            {
+                var orderDetail = context.Set<OrderDetail>().Find(ItemID);
+
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                
+                Attachment data = new Attachment(HostingEnvironment.ApplicationPhysicalPath + "Uploads/EcardFiles/" + orderDetail.Ecard.EcardUrl);
+                mail.Attachments.Add(data);
+                
+
+                mail.From = new MailAddress("lethanh.hlht1993@gmail.com");
+                mail.To.Add(orderDetail.Order.RecipientEmail);
+                mail.Subject = orderDetail.Order.SendSubject + " - " + orderDetail.Order.SenderName;
+                mail.Body = orderDetail.Order.SendMessage;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("lethanh.hlht1993@gmail.com", "*******");
+                SmtpServer.EnableSsl = true;
+
+                try
+                {
+                    //SmtpServer.Send(mail);
+                    orderDetail.SendStatus = true;
+                    orderDetail.SendTime = DateTime.Now;
+                    context.Set<OrderDetail>().Attach(orderDetail);
+                    context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        public async static void SendMailAll(List<int> ItemID)
+        {
+            foreach (var item in ItemID)
+            {
+                SendMailByOrder(item);
+            }
+            
         }
     }
 }
